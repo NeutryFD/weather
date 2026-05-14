4a4125170b478e96deaac76632b872343cefc796
neutry <luisangel20.las@gmail.com>
2026-05-14 17:58:06 +0200

    Add weather script using wttr.in with ip-api geolocation

---
+++ weather.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+WTTR_PARAMS="${WTTR_PARAMS:-n}"
+
+LOCATION=$(curl -s http://ip-api.com/csv/?fields=city,lat,lon)
+CITY="${LOCATION%%,*}"
+GEO="${LOCATION#*,}"
+
+wttr() {
+  local location params=() args=()
+
+  for arg in "$@"; do
+    if [[ "$arg" != -* && -z "$location" ]]; then
+      location="${arg// /+}"
+    else
+      params+=("${arg#-}")
+    fi
+  done
+
+  for p in $WTTR_PARAMS "${params[@]}"; do
+    if [[ "$p" == "s" ]]; then
+      args+=("--data-urlencode" "format=3")
+    else
+      args+=("--data-urlencode" "$p")
+    fi
+  done
+
+  curl -fGsS -H "Accept-Language: ${LANG%_*}" "${args[@]}" --compressed "wttr.in/${location:-$GEO}"
+}
+
+echo -ne "$CITY: " && wttr "$@"
